0188ca2f0d74cc28bd64db67927fa3a66925398c
MetaBot-Crypto&WebGL <80860637+sayhellometaverse2022@users.noreply.github.com>
2022-05-28 08:44:34 -0500

    update

--- index.js
+++ index.js
@@ -399,21 +399,21 @@ export default e => {
   
   let wearing = false;
   useWear(e => {
-    const {wear} = e;
-    // for (const subApp of subApps) {
-    //   subApp.position.copy(app.position);
-    //   subApp.quaternion.copy(app.quaternion);
-    //   subApp.scale.copy(app.scale);
-    //   subApp.updateMatrixWorld();
+    const {wear, player} = e;
+    for (const subApp of subApps) {
+      subApp.position.copy(app.position);
+      subApp.quaternion.copy(app.quaternion);
+      subApp.scale.copy(app.scale);
+      subApp.updateMatrixWorld();
       
-    //   subApp.dispatchEvent({
-    //     type: 'wearupdate',,
-    //     app: subApp,
-    //     player:
-    //     wear,
-    //   });
-    // }
-    wearing = !!wear;
+      subApp.dispatchEvent({
+        type: 'wearupdate',,
+        app: subApp,
+        player:
+        wear,
+      });
+    }
+    wearing = wear;
   });
   
   useUse(e => {
